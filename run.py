db43f01b714135b955191128c912de8afff031b5
Dabronzo <ayrtonterceiro@gmail.com>
2022-01-02 10:35:57 +0000

    Calculate and return the surplus rowl

--- run.py
+++ run.py
@@ -67,8 +67,15 @@ def calculate_surplus_data(sales_row):
     print("Calculating surplus data..\n")
     stock = SHEET.worksheet('stock').get_all_values()
     stock_rowll = stock[-1]
-    print(stock_rowll)
-    
+
+    surplus_data = []
+    for stock, sales in zip(stock_rowll, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+
+    return surplus_data
+
+
 
 
 
@@ -80,7 +87,8 @@ def main():
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
-    calculate_surplus_data(sales_data)
+    new_surplus_data = calculate_surplus_data(sales_data)
+    print(new_surplus_data)
 
 
 print("Welcome to LoveSandwiches Data Automation.\n")
